ab32a313f01bafa0e71c3ac79e2f8aa2cbc79298
edwin <94605646@qq.com>
2026-01-30 03:49:55 +0800

    clean up

--- coda/__init__.py
+++
@@ -1,3 +0,0 @@
-from .coda import CODA
-from .datasets import Dataset
-from .oracle import Oracle
--- coda/baselines/__init__.py
+++
@@ -1,5 +0,0 @@
-from .iid import IID
-from .activetesting import ActiveTesting
-from .vma import VMA
-from .modelpicker import ModelPicker
-from .uncertainty import Uncertainty
